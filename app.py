769df75de0bb575ff5e11f6ab67d19145eda0612
murahari1 <133811613+murahari1@users.noreply.github.com>
2025-05-02 21:46:17 +0530

    added register page

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def login():
-    return render_template('index.html')
-
+    return render_template('login.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
 if __name__ == "__main__":
     app.run(debug=True)
